7f9bb75d32eea7152ad215a415bedfdc170e663a
Burdell FitzGerald <cbf@email.sc.edu>
2017-12-04 18:23:08 -0500

    new model added... all for not, will have to create new solution

--- TumorTaskforce-Research/TumorTaskforce-Research/patient.cs
+++ TumorTaskforce-Research/TumorTaskforce-Research/patient.cs
@@ -12,26 +12,47 @@ namespace TumorTaskforce_Research
     using System;
     using System.Collections.Generic;
     
-    public partial class patient
+    public partial class Patient
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public patient()
+        public Patient()
         {
-            this.applied_treatments = new HashSet<applied_treatments>();
+            this.FamilyHistoryPivots = new HashSet<FamilyHistoryPivot>();
+            this.HealthFactorsPivots = new HashSet<HealthFactorsPivot>();
+            this.OtherMedsPivots = new HashSet<OtherMedsPivot>();
+            this.SymptomsPivots = new HashSet<SymptomsPivot>();
+            this.TreatmentsPivots = new HashSet<TreatmentsPivot>();
         }
     
-        public long patients_ID { get; set; }
-        public string First_Name { get; set; }
-        public string Last_Name { get; set; }
-        public System.DateTime DOB { get; set; }
-        public sbyte Sex_Male { get; set; }
-        public string Diagnosis { get; set; }
-        public long Medical_Record { get; set; }
-        public long Family_History { get; set; }
+        public int patientID { get; set; }
+        public string Sex { get; set; }
+        public bool Married { get; set; }
+        public byte Age { get; set; }
+        public string HistologicalClassification { get; set; }
+        public Nullable<byte> HistologicalGrade { get; set; }
+        public Nullable<short> TumorWidth { get; set; }
+        public Nullable<short> TumorHeight { get; set; }
+        public Nullable<short> TumorLength { get; set; }
+        public string TumorLocation { get; set; }
+        public string Constitutional { get; set; }
+        public string Respiratory { get; set; }
+        public string Cardiovascular { get; set; }
+        public string Gastrointestinal { get; set; }
+        public string Musculoskeletal { get; set; }
+        public string Integumentary { get; set; }
+        public string Neurologic { get; set; }
+        public string Exercize { get; set; }
+        public string Diet { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<applied_treatments> applied_treatments { get; set; }
-        public virtual record record { get; set; }
-        public virtual record record1 { get; set; }
+        public virtual ICollection<FamilyHistoryPivot> FamilyHistoryPivots { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<HealthFactorsPivot> HealthFactorsPivots { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<OtherMedsPivot> OtherMedsPivots { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SymptomsPivot> SymptomsPivots { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<TreatmentsPivot> TreatmentsPivots { get; set; }
     }
 }
