09d08022a8b26d9b06bcb22288d34fa9fd2919cc
bsand <bsand@DESKTOP-GUP0A34>
2017-12-04 22:36:27 -0500

    testing to make sure picture failure was corrected

--- TumorTaskforce_Webapp_1/TumorTaskforce_Webapp_1/Controllers/PossibleSymptomsController.cs
+++ TumorTaskforce_Webapp_1/TumorTaskforce_Webapp_1/Controllers/PossibleSymptomsController.cs
@@ -8,6 +8,8 @@ using System.Web;
 using System.Web.Mvc;
 using TumorTaskforce_Webapp_1;
 
+
+
 namespace TumorTaskforce_Webapp_1.Controllers
 {
     public class PossibleSymptomsController : Controller
