8f1e67d5c96c3b06552f9cba77be69f23ddfa486
LAURA GLESSNER <laurag@email.sc.edu>
2018-02-03 14:03:45 -0500

    Unit Test Creation

---
+++ TumorTaskforce_Webapp_1/StringLibraryTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+				/*int x = 5;
+				int y = -2;
+				int expectedResult = 3;
+
+				Calculator calculator = new Calculator();
+				int actualResult = calculator.Add(x, y);
+				Assert.AreEqual(expectedResult, actualResult);
+				*/
+
+				
+
+
+		}
+    }
+}
