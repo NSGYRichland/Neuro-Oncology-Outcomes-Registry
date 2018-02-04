582b65175973401dc8b0008d6aab71a704495605
bsand <bsand@DESKTOP-GUP0A34>
2018-02-04 15:43:27 -0500

    behaviorhal test attempt 1

--- TumorTaskforce_Webapp_1/StringLibraryTest/UnitTest1.cs
+++ TumorTaskforce_Webapp_1/StringLibraryTest/UnitTest1.cs
@@ -1,25 +1,44 @@
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
 
 namespace StringLibraryTest
 {
     [TestClass]
     public class UnitTest1
     {
+
+        //ChromeDriver driver = new ChromeDriver();
+        FirefoxDriver driver = new FirefoxDriver();
+
         [TestMethod]
-        public void TestMethod1()
+        public void SearchPatient()
         {
-				/*int x = 5;
-				int y = -2;
-				int expectedResult = 3;
 
-				Calculator calculator = new Calculator();
-				int actualResult = calculator.Add(x, y);
-				Assert.AreEqual(expectedResult, actualResult);
-				*/
-
-				
-
+            try
+            {
+                string url = "http://localhost:52612/";
+                //ChromeDriver driver = new ChromeDriver();
+                driver.Navigate().GoToUrl(url);
+                driver.Manage().Window.Maximize();
+                driver.FindElement(By.Id("txtBox")).SendKeys("3");
+                driver.FindElement(By.Id("buttonSubmit")).Click();
+                driver.Close();
+                driver.Dispose();
+            }
+            catch
+            {
+                ITakesScreenshot screenshotDriver = driver as ITakesScreenshot;
+                Screenshot screenshot = screenshotDriver.GetScreenshot();
+                screenshot.SaveAsFile("D:/test.png", ScreenshotImageFormat.Png);
+                driver.Quit();
+            }
 
 		}
+
     }
 }
