cebbe393185cdcb9c633bbb709d7707a12e8c404
Burdell FitzGerald <Burdell.FitzGerald@vc3.com>
2018-02-28 02:55:58 -0500

    Continued RBAC - user deleting, editing, etc.

--- TumorTaskforce_Webapp_1/TumorTaskforce_Webapp_1/Controllers/UsersController.cs
+++ TumorTaskforce_Webapp_1/TumorTaskforce_Webapp_1/Controllers/UsersController.cs
@@ -27,14 +27,22 @@ namespace TumorTaskforce_Webapp_1.Controllers
                     ViewBag.displayMenu = "Yes";
                 }
             }
-            ViewBag.userMGR = UserManager;
-            ViewBag.userlist = UserManager.Users.ToList();
             ViewBag.currentUserID = User.Identity.GetUserId();
-            return View();
+            IEnumerable<UserViewModel> users = Enumerable.Empty<UserViewModel>();
+            foreach (var item in UserManager.Users.ToList())
+            {
+                UserViewModel t = new UserViewModel();
+                t.Email = item.Email;
+                t.Id = item.Id;
+                t.Role = UserManager.GetRoles(item.Id).ToArray()[0];
+                t.UserName = item.UserName;
+                users = users.Concat(new[] { t });
+            }
+            return View(users);
         }
         public ActionResult Edit(string id)
         {
-            if (id.Equals(null))
+            if (id==null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
@@ -53,13 +61,56 @@ namespace TumorTaskforce_Webapp_1.Controllers
                     ViewBag.displayMenu = "Yes";
                 }
             }
-            ViewBag.userMGR = UserManager;
-            ViewBag.user = theUser;
-            return View();
+            UserViewModel t = new UserViewModel();
+            t.Email = theUser.Email;
+            t.Id = theUser.Id;
+            t.Role = UserManager.GetRoles(theUser.Id).ToArray()[0];
+            t.UserName = theUser.UserName;
+            ViewBag.roles = new SelectList(context.Roles.ToList(), "Name", "Name");
+            return View(t);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async System.Threading.Tasks.Task<ActionResult> Edit([Bind(Include = "Id,UserName,Email,Role")] UserViewModel user)
+        {
+            if (user.Role == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationDbContext context = new ApplicationDbContext();
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            await UserManager.RemoveFromRolesAsync(user.Id, UserManager.GetRolesAsync(user.Id).Result.ToArray());
+            await UserManager.AddToRoleAsync(user.Id, user.Role);
+            return RedirectToAction("Index");
+        }
+
+        //public async System.Threading.Tasks.Task<ActionResult> SaveAsync(string user, SelectList roles)
+        //{
+        //    if (user==null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    ApplicationDbContext context = new ApplicationDbContext();
+        //    var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+        //    var theUser = UserManager.Users.ToList().Find(i => i.Id.Equals(user));
+        //    string role = roles.SelectedValue.ToString();
+        //    if (theUser == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    if (role==null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    await UserManager.RemoveFromRolesAsync(user, UserManager.GetRolesAsync(user).Result.ToArray());
+        //    await UserManager.AddToRoleAsync(user, role);
+        //    return RedirectToAction("Index");
+        //}
+
         public ActionResult Delete(string id)
         {
-            if (id.Equals(null))
+            if (id==null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
@@ -70,9 +121,9 @@ namespace TumorTaskforce_Webapp_1.Controllers
             {
                 return HttpNotFound();
             }
-            if (User.Identity.GetUserId().Equals(id))
+            if (User.Identity.GetUserId().Equals(id)||UserManager.FindById(id).UserName.Equals("c.burdell"))
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return new HttpStatusCodeResult(HttpStatusCode.Conflict);
             }
             else
             {
